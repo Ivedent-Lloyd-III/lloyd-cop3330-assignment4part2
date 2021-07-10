7b71c3c0af1d709da70e5baa246a6fe3790f1f61
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-10 18:13:13 -0400

    initial commit

---
+++ src/main/java/ucf/assignments/ListMakerController.java
@@ -0,0 +1,22 @@
+package ucf.assignments;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ListMakerController {
+
+    @FXML
+    public void addNewItemButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void newItemTitleTextEntered(ActionEvent actionEvent) {
+    }
+
+    public void newItemDescriptionTextEntered(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/ucf/assignments/todoListItemsTable.java
@@ -0,0 +1,9 @@
+package ucf.assignments;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+public class todoListItemsTable {
+}
