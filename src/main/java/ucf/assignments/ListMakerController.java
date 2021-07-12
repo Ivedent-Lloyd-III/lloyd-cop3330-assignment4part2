e7949af140a497547f73832cbd02846fe03dc162
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-11 20:25:50 -0400

    initial commit

--- src/main/java/ucf/assignments/ListMakerController.java
+++ src/main/java/ucf/assignments/ListMakerController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,19 +23,19 @@ import java.util.ResourceBundle;
 public class ListMakerController implements Initializable {
 
     @FXML
-    private TableView<listItems> tableView;
+    private TableView<items> tableView;
 
     @FXML
-    private TableColumn<listItems, String> itemsStatusColumn;
+    private TableColumn<items, String> itemsStatusColumn;
 
     @FXML
-    private TableColumn<listItems, String> itemDueDateColumn;
+    private TableColumn<items, String> itemDueDateColumn;
 
     @FXML
-    private TableColumn<listItems, String> itemsNameColumn;
+    private TableColumn<items, String> itemsNameColumn;
 
     @FXML
-    private TableColumn<listItems, String> itemsDescriptionColumn;
+    private TableColumn<items, String> itemsDescriptionColumn;
 
     @FXML
     private TextField newItemTitleTextField;
@@ -52,24 +53,77 @@ public class ListMakerController implements Initializable {
     private Button deleteItemButton;
     
 
-    ObservableList<listItems> list = FXCollections.observableArrayList(
-            new listItems("incompleted", "071321", "Wash Dog", "Wash the dog with shampoo"),
-            new listItems("completed", "071321", "Wash Car", "wash the car with soap")
+    ObservableList<items> list = FXCollections.observableArrayList(
+            new items("incompleted", "071321", "Wash Dog", "Wash the dog with shampoo"),
+            new items("completed", "071321", "Wash Car", "wash the car with soap")
     );
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        itemsStatusColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemStatus"));
-        itemDueDateColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemDueDate"));
-        itemsNameColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemName"));
-        itemsDescriptionColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemDescription"));
+        loadTableSampleData();
+        initializeTable();
 
-        tableView.setItems(list);
+       // itemsStatusColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemStatus"));
+       // itemDueDateColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemDueDate"));
+       // itemsNameColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemName"));
+       // itemsDescriptionColumn.setCellValueFactory(new PropertyValueFactory<listItems, String>("itemDescription"));
+
+       // tableView.setItems(list);
+    }
+
+    public void initializeTable(){
+        initializeColumns();
+    }
+
+    public void initializeColumns(){
+
+        itemsStatusColumn.setCellValueFactory(new PropertyValueFactory<items, String>("itemStatus"));
+        itemDueDateColumn.setCellValueFactory(new PropertyValueFactory<items, String>("itemDueDate"));
+        itemsNameColumn.setCellValueFactory(new PropertyValueFactory<items, String>("itemName"));
+        itemsDescriptionColumn.setCellValueFactory(new PropertyValueFactory<items, String>("itemDescription"));
+
+        editColumns();
+    }
+
+    public void editColumns(){
+
+        itemsStatusColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        itemsStatusColumn.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setItemStatus(e.getNewValue());
+        });
+        itemDueDateColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        itemDueDateColumn.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setItemDueDate(e.getNewValue());
+        });
+        itemsNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        itemsNameColumn.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setItemName(e.getNewValue());
+        });
+        itemsDescriptionColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        itemsDescriptionColumn.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setItemDescription(e.getNewValue());
+        });
+
+        tableView.setEditable(true);
+
+    }
+
+    public void loadTableSampleData(){
+
+        ObservableList<items> sampleData = FXCollections.observableArrayList();
+        sampleData.add(new items("incompleted", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+        sampleData.add(new items("incompleted", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+        sampleData.add(new items("completed", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+        sampleData.add(new items("completed", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+        sampleData.add(new items("completed", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+        sampleData.add(new items("completed", "2021-07-11", "Assignment 4", "design a gui that makes a todo list"));
+
+        tableView.setItems(sampleData);
     }
 
     public void addButtonClicked(ActionEvent actionEvent) {
-        listItems newItem = new listItems();
+        items newItem = new items();
         newItem.setItemDueDate(newItemDueDateTextField.getText());
         newItem.setItemName(newItemTitleTextField.getText());
         newItem.setItemDescription(newItemDescriptioinTextField.getText());
@@ -83,7 +137,7 @@ public class ListMakerController implements Initializable {
 
     public void deleteButtonClicked(ActionEvent actionEvent) {
 
-        ObservableList<listItems> itemSelected, allItems;
+        ObservableList<items> itemSelected, allItems;
         allItems = tableView.getItems();
         itemSelected = tableView.getSelectionModel().getSelectedItems();
 
--- src/main/java/ucf/assignments/items.java
+++ src/main/java/ucf/assignments/items.java
@@ -1,68 +1,57 @@
 package ucf.assignments;
 
-/*
- *  UCF COP3330 Summer 2021 Assignment 4 Solution
- *  Copyright 2021 Ivedent Lloyd III
- */
-
-import javafx.beans.property.SimpleStringProperty;
 import java.time.LocalDate;
 
 public class items {
 
-    private SimpleStringProperty itemName;
-    private SimpleStringProperty itemDescription;
-    private LocalDate itemDueDate;
-    private SimpleStringProperty itemStatus;
+    private String itemName;
+    private String itemDescription;
+    private String itemStatus;
+    private String itemDueDate;
 
-    public items(String itemName, String itemDescription, LocalDate itemDueDate, String itemStatus) {
-        this.itemName = new SimpleStringProperty(itemName);
-        this.itemDescription = new SimpleStringProperty(itemDescription);
+    public items(){
+        this.itemName = "";
+        this.itemDescription = "";
+        this.itemStatus = "";
+        this.itemDueDate = "";
+    }
+
+    public items(String itemStatus, String itemDueDate, String itemName, String itemDescription) {
+        this.itemStatus = itemStatus;
         this.itemDueDate = itemDueDate;
-        this.itemStatus = new SimpleStringProperty(itemStatus);
-    }
-
-    public String getItemName() {
-        return itemName.get();
-    }
-
-    public SimpleStringProperty itemNameProperty() {
-        return itemName;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
     }
 
     public void setItemName(String itemName) {
-        this.itemName = new SimpleStringProperty(itemName);
-    }
-
-    public String getItemDescription() {
-        return itemDescription.get();
-    }
-
-    public SimpleStringProperty itemDescriptionProperty() {
-        return itemDescription;
+        this.itemName = itemName;
     }
 
     public void setItemDescription(String itemDescription) {
-        this.itemDescription = new SimpleStringProperty(itemDescription);
-    }
-
-    public LocalDate getItemDueDate() {
-        return itemDueDate;
-    }
-
-    public void setItemDueDate(LocalDate itemDueDate) {
-        this.itemDueDate = itemDueDate;
-    }
-
-    public String getItemStatus() {
-        return itemStatus.get();
-    }
-
-    public SimpleStringProperty itemStatusProperty() {
-        return itemStatus;
+        this.itemDescription = itemDescription;
     }
 
     public void setItemStatus(String itemStatus) {
-        this.itemStatus = new SimpleStringProperty(itemStatus);
+        this.itemStatus = itemStatus;
+    }
+
+    public void setItemDueDate(String itemDueDate) {
+        this.itemDueDate = itemDueDate;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public String getItemStatus() {
+        return itemStatus;
+    }
+
+    public String getItemDueDate() {
+        return itemDueDate;
     }
 }
